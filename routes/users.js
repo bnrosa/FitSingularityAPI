ff4e4aa3cf6e4d21c771dd0ae4c5aceaeddfe736
Bernardo <bernardoorosa@gmail.com>
2020-07-14 13:52:18 -0300

    :tada: Initial commit

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) =>{
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) =>{
+    User.findById(req.params.id)
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) =>{
+    User.findByIdAndDelete(req.params.id)
+        .then(user => res.json('User deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) =>{
+    User.findById(req.params.id)
+        .then(user => {
+            user.username = req.body.username,
+
+            user.save()
+                .then(() => res.json('User updated!'))
+                .catch(err => res.status(400).json('Error: '+ err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const newUser = new User({
+        username: req.body.username
+    });
+    newUser.save()
+        .then(() => res.json('User added'))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect( uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', usersRouter);
+app.use('/exercises', exercisesRouter);
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}`);
+});
