7a4c94183c02899b9efcd8654d089cb91757147a
Bernardo <bernardoorosa@gmail.com>
2020-07-16 14:04:45 -0300

    :lock: Added auth middleware to users and exercises routes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect( uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
@@ -26,9 +27,11 @@ require("./config/passport")(passport);
 
 const exercisesEntriesRouter = require('./routes/exercise-entries');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
-app.use('/users', usersRouter);
-app.use('/exercises', exercisesEntriesRouter);
+app.use('/users', passport.authenticate('jwt', {session: false}), usersRouter);
+app.use('/exercises-entries', passport.authenticate('jwt', {session: false}), exercisesEntriesRouter);
+app.use('/auth', authRouter)
 
 app.listen(port, () =>{
     console.log(`Server running on port ${port}`);
